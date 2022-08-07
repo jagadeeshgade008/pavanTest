bf6d28989ca2de51d44f3e54596121df6dde1851
jagadeeshgade008 <79077571+jagadeeshgade008@users.noreply.github.com>
2022-08-07 10:17:35 -0500

    add

--- test.js
+++ test.js
@@ -19,47 +19,8 @@
                 },
                 success: function (data) {
 
-                    // prepare the object as inputs of the form with type as text
-
-                    var inputs = "";
-                    for (var key in data.data) {
-
-                        // each input is a div with class form-group and contains a label and input field with name as key and value as data[key] and with column of 6
-                        inputs += "<div class='form-group col-md-6'>" +
-                            "<label for='" + key + "'>" + key + "</label>" +
-                            "<input type='text' class='form-control' name='" +
-                            key + "' value='" + data.data[key] + "'>" +
-                            "</div>";
-
-                        // inputs += "<input type='text' class='form-control' name='" + key + "' value='" + data.data[
-                        //     key] + "'>";
-                    }
-
-                    // add update button
-                    inputs +=
-                        "<button type='submit' class='btn btn-primary' id='update' name='update'>Update</button>";
-
-                    // append the inputs to the form
-                    $("#updateDetailsContainer").append(inputs);
-
-                    $("#update").click(function () {
-                        var data = {};
-                        $("#resultsContainer input").each(function () {
-                            data[$(this).attr("name")] = $(this).val();
-                        });
-                        console.log(data);
-
-                        // post data to api
-                        $.ajax({
-                            url: "/apitocall",
-                            method: "POST",
-                            data: data,
-                            success: function (data) {
-                                console.log(data);
-                            }
-                        });
-
-                    });
+                    
+                 
 
                 }
             });
